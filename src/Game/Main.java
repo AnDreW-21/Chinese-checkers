d488f3c97a58ccab65813b6bfab8bd819b443c18
phoebe-george <phoebegeorge137@gmail.com>
2022-06-01 19:32:11 +0200

    add update and jump

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -3,12 +3,15 @@ class Main {
     public static void main(String[] args) {
         Moves myMove = new Moves();
         int [][] availableMove = new int [6][2];
-        int MyRow=6,MyColum=10;
+        int MyRow=3,MyColum=11;
         availableMove = myMove.AllAvailableMoves(MyRow,MyColum);
         for (int i=0;i<6;i++){
             System.out.println("row "+availableMove[i][0]+" colum "+availableMove[i][1]);
         }
 
+        myMove.printBoard();
+        boolean flag = myMove.Jumpcheck(MyRow,MyColum,5,13);
+        System.out.println(flag);
         myMove.printBoard();
     }
 }
--- src/Game/Moves.java
+++ src/Game/Moves.java
@@ -9,7 +9,7 @@ public class Moves {
     public Moves (){
         Board = new createBoard();
         board = Board.getBoard();
-//        board[6][8]='1';
+        board[4][12]='1';
 //        Board.displayWithoutDashAndNums();
     }
     public int[][] AllAvailableMoves(int MyRow,int MyColum){
@@ -79,5 +79,26 @@ public class Moves {
     }
     public void printBoard(){
         Board.displayWithoutDashAndNums();
+    }
+    public void updateBoard(int MyRow,int MyColum,int MyNewRow,int MyNewColum){
+        board[MyNewRow][MyNewColum] = board[MyRow][MyColum];
+        board[MyRow][MyColum] ='O';
+    }
+    public boolean Jumpcheck(int MyRow,int MyColum,int MyNewRow,int MyNewColum){
+        updateBoard(MyRow,MyColum,MyNewRow,MyNewColum);
+        if(MyNewRow == MyRow-2 && MyNewColum == MyColum-2){// up_left jump
+         return true;
+        }else if(MyNewRow == MyRow-2 && MyNewColum == MyColum+2){//up_right jump
+            return true;
+        }else if(MyNewRow == MyRow+2 && MyNewColum == MyColum-2){//down_left jump
+            return true;
+        }else if(MyNewRow == MyRow+2 && MyNewColum == MyColum+2){//down_right jump
+            return true;
+        }else if(MyNewRow == MyRow && MyNewColum==MyColum+4){//right jump
+            return true;
+        }else if(MyNewRow == MyRow && MyNewColum==MyColum-4){//left jump
+            return true;
+        }
+        return false;
     }
 }
